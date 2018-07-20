a1b873005a428531bf48bb55d13b212ab319fd18
wqh17101 <597935261@qq.com>
2018-07-20 21:16:10 +0800

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ sys.path.insert(0, os.path.abspath('../../MatchZoo/matchzoo'))
 sys.path.insert(0, os.path.abspath('../../MatchZoo/matchzoo/engine'))
 sys.path.insert(0, os.path.abspath('../../MatchZoo/matchzoo/model'))
 sys.path.insert(0, os.path.abspath('../../MatchZoo/matchzoo/task'))
+sys.path.insert(0, os.path.abspath('../../MatchZoo/matchzoo/preprocessor'))
 
 
 # -- Project information -----------------------------------------------------
@@ -67,7 +68,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh'
 locale_dirs = ['locale/']   # path is example but recommended.
 gettext_compact = False
 # List of patterns, relative to source directory, that match files and
